d00c853a5a77922a83c4d21eb581473a3984aecf
hollyjon9-dot <hollyjon9@gmail.com>
2026-04-19 20:07:58 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+      console.log("Service worker aktif");
+    } catch (err) {
+      console.error("Gagal register service worker:", err);
+    }
+  });
+}
