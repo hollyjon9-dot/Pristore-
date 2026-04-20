fc4ca7a627a2e2129e54a81c3f94dd3b7c710cd9
hollyjon9-dot <hollyjon9@gmail.com>
2026-04-20 19:50:32 +0900

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,46 +1,24 @@
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDef4V0YWxFLVNKjY8qWXPld6iLR9jWpcE",
   authDomain: "pristore-3cd7d.firebaseapp.com",
   projectId: "pristore-3cd7d",
+  messagingSenderId: "521855008847",
+  appId: "1:521855008847:web:bb0bf7c8f758b8a7c16a3a",
 });
 
 const messaging = firebase.messaging();
 
-// NOTIF SAAT APP DI BACKGROUND / CLOSE
 messaging.onBackgroundMessage(function (payload) {
-  console.log("Background notif:", payload);
+  console.log("Notif background:", payload);
 
-  const title = payload.notification?.title || "Pristore";
+  const title = payload.notification.title;
   const options = {
-    body: payload.notification?.body || "Ada notifikasi baru",
+    body: payload.notification.body,
     icon: "/logo192.png",
-    badge: "/badge.png",
-    vibrate: [200, 100, 200],
-    data: {
-      url: "/",
-    },
   };
 
   self.registration.showNotification(title, options);
-});
-
-// KLIK NOTIF → BUKA WEBSITE
-self.addEventListener("notificationclick", function (event) {
-  event.notification.close();
-
-  event.waitUntil(
-    clients.matchAll({ type: "window" }).then((clientList) => {
-      for (const client of clientList) {
-        if (client.url === "/" && "focus" in client) {
-          return client.focus();
-        }
-      }
-      if (clients.openWindow) {
-        return clients.openWindow("/");
-      }
-    })
-  );
 });
